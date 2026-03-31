14149411efa692a1e698516fcba41d6c04b3e156
Neeraj S <neerajms2009@gmail.com>
2026-03-31 12:59:47 +0530

    v1 Web-Shooter device firmware

---
+++ Web-Ignition.ino
@@ -0,0 +1,40 @@
+const int button = 7;
+const int motor = 11;
+const int first_solenoid = 8;
+const int second_solenoid = 9;
+const int third_solenoid = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(11,OUTPUT); // Motor Pin
+
+  pinMode(8,OUTPUT); // solenoid-1
+  pinMode(9,OUTPUT); // solenoid-2
+  pinMode(10,OUTPUT); // solenoid-3
+
+  pinMode(7,INPUT); // button(trigger)
+
+
+}
+
+void loop() {
+  //turning on the first solenoid
+  digitalWrite(first_solenoid,true);
+  delay(5000);
+  //second solenoid 
+  digitalWrite(second_solenoid,true);
+  delay(5000);
+  //third solenoid
+  digitalWrite(third_solenoid,true);
+
+  if(button == HIGH){
+    digitalWrite(motor,true);
+    delay(60000); 
+    digitalWrite(motor,false);
+  }else{
+    digitalWrite(motor,false);
+  }
+  
+
+
+}
