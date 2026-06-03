66af74c7e2b0c9968488fc312e9288e48ace6196
Neeraj Shibu <neerajms2009@gmail.com>
2026-06-03 15:45:22 +0530

    Embedded with 2-3 features than v3

--- Web-Ignition.ino
+++ Web-Ignition.ino
@@ -1,40 +1,106 @@
 const int button = 7;
 const int motor = 11;
-const int first_solenoid = 8;
-const int second_solenoid = 9;
-const int third_solenoid = 10;
+ 
+///Three transistors for each solenoids/////
+const int transistorGate_1 = 12;  // Transistor_1 is wired with first solenoid
+const int transistorGate_2 = 14;  // Transistor_2 is wired with second solenoid
+const int transistorGate_3 = 18;  // Transistor_3 is wired with thrid solenoid
+
+
+unsigned long start_time = 0;
+const long motor_duration = 60400;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(11,OUTPUT); // Motor Pin
+                                ////////////////////Initializing Components/////////////////////////
 
-  pinMode(8,OUTPUT); // solenoid-1
-  pinMode(9,OUTPUT); // solenoid-2
-  pinMode(10,OUTPUT); // solenoid-3
+  pinMode(motor,OUTPUT);    // Motor Pin
 
-  pinMode(7,INPUT); // button(trigger)
+  pinMode(transistorGate_1,OUTPUT);  
+  pinMode(transistorGate_2,OUTPUT);
+  pinMode(transistorGate_3, OUTPUT);
+
+  pinMode(button,INPUT_PULLUP);  // button(trigger)
+
+                                 ///////////////////Assigning all components to LOW, for safety/////////////////////////
+   
+  digitalWrite(transistorGate_1, LOW);   
+  digitalWrite(transistorGate_2, LOW);
+  digitalWrite(transistorGate_3, LOW);
+  
+  digitalWrite(motor, LOW);
+
+  start_time = millis();     //initiating time
 
 
 }
 
-void loop() {
-  //turning on the first solenoid
-  digitalWrite(first_solenoid, HIGH);
-  delay(150);
-  //second solenoid 
-  digitalWrite(second_solenoid, HIGH);
-  delay(150);
-  //third solenoid
-  digitalWrite(third_solenoid, HIGH);
 
-  if(digitalRead(button) == HIGH){
-    digitalWrite(motor,HIGH);
-    delay(60000); 
-    digitalWrite(motor, LOW);
-  }else{
-    digitalWrite(motor, LOW);
-  }
+void loop() {  
   
+    if (start_time > 0) {
+      unsigned long elapsed = millis() - start_time;    //continously stores elapsed time    
 
+      
+
+      ///Sequential Initialization of Solenoids///
+
+      if(elapsed >= 100) {
+        digitalWrite(transistorGate_1, HIGH);
+      }else {
+        digitalWrite(transistorGate_1, LOW);
+      }
+
+      if(elapsed >= 200) {
+        digitalWrite(transistorGate_2, HIGH);
+      } else {
+        digitalWrite(transistorGate_2, LOW);
+      }
+
+      if(elapsed >= 300) {
+        digitalWrite(transistorGate_3, HIGH);
+      }else{
+        digitalWrite(transistorGate_3, LOW);
+      } 
+
+      if(elapsed >= 5000){
+        TurnOffTransistors();  //Transistors turns off, after 5 seconds of INACTIVITY
+      }
+
+
+       int buttonState = digitalRead(button);
+
+       if(buttonState == LOW && elapsed >= 400 && elapsed < motor_duration) {
+        digitalWrite(motor, HIGH);
+       }else {
+        digitalWrite(motor, LOW);
+       }
+
+       if(elapsed >= motor_duration){
+        digitalWrite(motor, LOW);
+        
+       }if((buttonState == HIGH) && (elapsed >= 60450)){
+        Kill_All();
+        start_time = 0;
+       }
+      
+            
+    } 
+
+} 
+  
+void TurnOffTransistors() {                       ////////////Turns off solenoids/transistors//////////////////
+  digitalWrite(transistorGate_1, LOW);
+  digitalWrite(transistorGate_2, LOW);
+  digitalWrite(transistorGate_3, LOW);
+}
+
+
+void Kill_All() {                              /////////////Kills everything//////////////////
+  digitalWrite(transistorGate_1, LOW);
+  digitalWrite(transistorGate_2, LOW);
+  digitalWrite(transistorGate_3, LOW);
+
+  digitalWrite(motor, LOW);
 
 }
